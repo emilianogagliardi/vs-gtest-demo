35d8714329a79c88885ad0d310f05d76600f037e
Emiliano Gagliardi <emilianogagliardi@icloud.com>
2020-03-25 17:06:32 +0100

    modified tests names

--- test/moduleA/sample1_unittest.cpp
+++ test/moduleA/sample1_unittest.cpp
@@ -1,25 +1,25 @@
 #include "moduleA/sample1.h"
 #include <gtest/gtest.h>
 
-TEST(AddTest, Zero)
+TEST(add, zero)
 {
     for(auto x : {10, 20, 11, 23, 1, 78})
         ASSERT_EQ(add(x, 0), x);
 }
-TEST(AddTest, Positive)
+TEST(add, positive)
 {
     ASSERT_EQ(add(1, 2), 3);
     ASSERT_EQ(add(2, 2), 4);
     ASSERT_EQ(add(2, 3), 5);
 }
-TEST(AddTest, Negative)
+TEST(add, negative)
 {
     ASSERT_EQ(add(1, -2), -1);
     ASSERT_EQ(add(-2, -2), -4);
     ASSERT_EQ(add(-2, 3), 1);
 }
 
-TEST(SubTest, Zero)
+TEST(sub, zero)
 {
     for (auto x : { 1, 2, 3, 4, 5 }) {
         ASSERT_EQ(sub(x, 0), x);
@@ -27,14 +27,14 @@ TEST(SubTest, Zero)
     }
 }
 
-TEST(SubTest, Equal)
+TEST(sub, equal)
 {
     for (auto x : { 1, 2, 3, 4, 5 }) {
         ASSERT_EQ(sub(x, x), 0);
     }
 }
 
-TEST(SubTest, General)
+TEST(sub, general)
 {
     ASSERT_EQ(sub(1, -2), 3);
     ASSERT_EQ(sub(3, 2), 1);
